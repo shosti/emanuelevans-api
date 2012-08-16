bde10adb3bd25fe0b38c8a14b34c7f6b337a0356
Phil Hagelberg <technomancy@gmail.com>
2012-08-16 09:38:00 -0700

    Change artifact id, drop plugin.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject clojure-force-sample "1.0.1"
+(defproject clojure-sample "1.0.1"
   :description "Hello World Clojure Web App"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
                  [ring/ring-jetty-adapter "1.1.2"]]
-  :plugins [[lein-ring "0.7.2"]]
-  :main sample.app)
+  :main ^:skip-aot sample.app)

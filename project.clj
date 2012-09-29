aeeebea372ef816a127cebb386c4f0e99eb19db9
Emanuel Evans <emanuel.evans@gmail.com>
2012-09-28 20:44:34 -0700

    Switch to Leiningen 2

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject clojure-sample "1.0.1"
+  :min-lein-version "2.0.0"
   :description "Hello World Clojure Web App"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.1"]
